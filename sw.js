f52bea524ee8f45631795681152434781a9ff4d4
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-08-23 01:32:01 +0800

    chore: bump cache for rewritten site

--- sw.js
+++ sw.js
@@ -1,19 +1,12 @@
-const CACHE_NAME = "chi-nav-v2";
+const CACHE_NAME = "chi-nav-v3";
 const CORE_ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./data.json", "./manifest.json"];
-
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-    return response;
-  }).catch(() => caches.match(event.request)));
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
