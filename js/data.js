6a57616ac62b37b5be14fcdfc4b160160f26d411
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-08-23 11:25:04 +0800

    refactor: extract data helpers

---
+++ js/data.js
@@ -0,0 +1,9 @@
+function load(k){try{const x=JSON.parse(localStorage.getItem(k)||'[]');return Array.isArray(x)?x:[]}catch{return[]}}
+function save(k,x){try{localStorage.setItem(k,JSON.stringify(x))}catch{}}
+function norm(u){try{const x=new URL(u,location.href);x.hash='';x.hostname=x.hostname.toLowerCase();x.pathname=x.pathname.replace(/\/{2,}/g,'/').replace(/\/$/,'')||'/';return x.toString()}catch{return String(u||'')}}
+function key(l){return norm(l.url)}
+function esc(v=''){return String(v).replace(/[&<>\"']/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','\"':'&quot;',"'":'&#039;'}[c]))}
+function src(n){return data.find(x=>x.taxonomy===n)}
+function flat(s){return s?(s.links?[...s.links]:(s.list||[]).flatMap(x=>x.links||[])):[]}
+function uniq(a){const seen=new Set();return a.filter(l=>{const k=key(l);if(seen.has(k))return false;seen.add(k);return true})}
+function tabData(g,t){const [label,source,index]=t,s=src(source);return uniq(s?.list&&index!=null?(s.list[index]?.links||[]):flat(s))}
