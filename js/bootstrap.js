54ef87a70433dd7f52259488f7851c2cd69a77ce
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-08-24 16:21:42 +0800

    chore: bump service worker version to v2.9.0

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator&&(location.protocol==='https:'||location.hostname==='localhost'))window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js?v=2.8.9',{updateViaCache:'none'}).catch(console.error));
+if('serviceWorker' in navigator&&(location.protocol==='https:'||location.hostname==='localhost'))window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js?v=2.9.0',{updateViaCache:'none'}).catch(console.error));
