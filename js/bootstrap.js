0d3cea278c54159aaa932e1cbfc2e65d4e91abe9
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-08-24 08:14:28 +0800

    test: sync bootstrap version to v2.7.9

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator&&(location.protocol==='https:'||location.hostname==='localhost'))window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js?v=2.7.8',{updateViaCache:'none'}).catch(console.error));
+if('serviceWorker' in navigator&&(location.protocol==='https:'||location.hostname==='localhost'))window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js?v=2.7.9',{updateViaCache:'none'}).catch(console.error));
