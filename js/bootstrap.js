48b04f630b6329ffeb3d3f90101ef19372633ca2
tsai97216 <123880738+tsai97216@users.noreply.github.com>
2026-08-28 14:59:01 +0800

    refactor: remove version query from service worker registration

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator&&(location.protocol==='https:'||location.hostname==='localhost'))window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js?v=3.0.0.4',{updateViaCache:'none'}).catch(console.error));
+if('serviceWorker' in navigator&&(location.protocol==='https:'||location.hostname==='localhost'))window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js',{updateViaCache:'none'}).catch(console.error));
